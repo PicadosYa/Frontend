f80f2d18b641da32534d884bbcd65d6d169504f5
Anthony Guekdjian <anthonyguekdjian@gmail.com>
2024-11-28 22:44:20 -0300

    improving header

--- src/components/register/HeaderSession.jsx
+++ src/components/register/HeaderSession.jsx
@@ -11,45 +11,68 @@ export function HeaderSession({ auth }) {
     const storedProfile = localStorage.getItem("userProfile");
     if (storedProfile) {
       const userProfileNoParsed = JSON.parse(storedProfile);
-      setFirstName(userProfileNoParsed.first_name); 
+      setFirstName(userProfileNoParsed.first_name);
     } else if (auth.firstname) {
-      setFirstName(auth.firstname); 
+      setFirstName(auth.firstname);
     }
   }, []);
-  const [isUserModalOpen, setIsUserModalOpen] = useState(false)
-  const [isUserProfileOpen, setIsUserProfileOpen] = useState(false)
+  const [isUserModalOpen, setIsUserModalOpen] = useState(false);
+  const [isUserProfileOpen, setIsUserProfileOpen] = useState(false);
+
+  const nameModified = (firstname, lastname) => {
+    const letters =
+      firstname.charAt(0).toUpperCase() + lastname.charAt(0).toUpperCase();
+    return letters;
+  };
   return (
     <div className="mx-5 bg-transparent rounded-[25px] p-4 flex justify-end items-center">
       <div
         className="flex space-x-6 items-center text-white"
         style={{ fontFamily: "Exo, sans-serif", lineHeight: "normal" }}
       >
-        <span>{firstName ? `Hola, ${firstName}` : `Hola, ${auth.firstname}`}!</span>
-        <div className="w-10 h-10 bg-gray-300 rounded-full flex justify-center items-center relative">
-          <img
-            src={auth.profile_picture_url ? auth.profile_picture_url : "/Proyecto nuevo 1.png"}
-            alt="Profile"
-            className="w-full h-full rounded-full cursor-pointer"
-            onClick={()=> setIsUserModalOpen(!isUserModalOpen)}
-          />
-          {isUserModalOpen && (
-
-            <div className="absolute top-[100%] w-44 z-10 bg-dark-blue rounded-md flex flex-col gap-2 p-2">
-              <h4 className="text-center font-bold">{firstName ? `Hola!, ${firstName}` : `Hola!, ${auth.firstname}`}</h4>
-              <button className="hover:font-bold text-left"
-                onClick={() => setIsUserProfileOpen(!isUserProfileOpen)}
-              >Actualizar perfil</button>
-              {auth.role === "client" ? null : (
-                <Link
-                  to="reservas"
-                  className="hover:font-bold"
-                >Reservas
-                </Link>
-              )}
-              
+        <span>
+          {firstName ? `Hola, ${firstName}` : `Hola, ${auth.firstname}`}!
+        </span>
+        <div className="w-11 h-11 bg-gray-300 rounded-full flex justify-center items-center relative">
+          {auth.profile_picture_url !== "default" ? (
+            <img
+              src={auth.profile_picture_url}
+              alt="Profile"
+              className="w-full h-full rounded-full cursor-pointer"
+              onClick={() => setIsUserModalOpen(!isUserModalOpen)}
+            />
+          ) : (
+            <div
+              className="w-full h-full border-[2.5px] border-solid border-white rounded-full bg-[#656A84] flex justify-center items-center font-semibold cursor-pointer"
+              onClick={() => setIsUserModalOpen(!isUserModalOpen)}
+            >
+              <span className="">
+                {nameModified(auth.firstname, auth.lastname)}
+              </span>
             </div>
           )}
-          </div>
+
+          {isUserModalOpen && (
+            <div className="absolute top-[100%] w-44 z-10 bg-dark-blue rounded-md flex flex-col gap-2 p-2">
+              <h4 className="text-center font-bold">
+                {firstName
+                  ? `Hola, ${firstName}!`
+                  : `Hola!, ${auth.firstname}!`}
+              </h4>
+              <button
+                className="hover:font-bold text-left"
+                onClick={() => setIsUserProfileOpen(!isUserProfileOpen)}
+              >
+                Actualizar perfil
+              </button>
+              {auth.role === "client" ? null : (
+                <Link to="reservas" className="hover:font-bold">
+                  Reservas
+                </Link>
+              )}
+            </div>
+          )}
+        </div>
 
         {/* <img src="../../../public/Hamburguer Menu.png" alt="" /> */}
         {auth.role == Global.rolesTypes.field ? (
@@ -63,10 +86,9 @@ export function HeaderSession({ auth }) {
           Cerrar Sesion
         </Link>
       </div>
-      {isUserProfileOpen &&(
-
+      {isUserProfileOpen && (
         <div className="fixed top-0 left-0 z-10 flex justify-center items-center min-w-full bg-dark-blue-opacity ">
-            <MyProfile setIsUserProfileOpen={setIsUserProfileOpen} />
+          <MyProfile setIsUserProfileOpen={setIsUserProfileOpen} />
         </div>
       )}
     </div>
